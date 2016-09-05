d23000c413c69142571c26dab92c0f161b9b8c0c
Dmytro Voznenko <dvoznenko@backbase.com>
2016-09-06 00:27:15 +0200

    add post method

--- SimpleHTTPServer.py
+++ SimpleHTTPServer.py
@@ -30,7 +30,19 @@ class HttpProcessor(BaseHTTPRequestHandler):
 				self.wfile.write("29")
 			return
 		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)	
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+	#Handler for the POST requests
+	def do_POST(self):
+		if self.path=="/post":
+			content_len = int(self.headers.getheader('content-length', 0))
+			post_body = self.rfile.read(content_len)
+
+			print post_body
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write("Thanks")
+			return			
 try:
 	#Create a web server and define the handler to manage the
 	#incoming request
